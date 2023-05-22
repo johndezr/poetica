3e5c51e35b2f91d593959e0a23b8c237bf68a96c
John <info@jgalindez.net>
2023-05-22 19:36:17 +0200

    basic mui configuration

---
+++ src/theme/base/index.tsx
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+import { createTheme } from "@mui/material/styles";
+import breakpoints from './breakpoints';
+import palette from "./palette";
+
+export const inter = Inter({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
+});
+
+// Create a theme instance.
+const defaultTheme = createTheme({
+  spacing: (factor: number) => `${0.5 * factor}rem`,
+  palette,
+  typography: {
+    fontFamily: inter.style.fontFamily,
+  },
+  breakpoints
+});
+
+export default defaultTheme;
+
---
+++ src/theme/base/palette.ts
@@ -0,0 +1,13 @@
+const palette =  {
+  primary: {
+    main: "#556cd6",
+  },
+  secondary: {
+    main: "#19857b",
+  },
+  error: {
+    main: "#ED0530",
+  },
+}
+
+export default palette;
